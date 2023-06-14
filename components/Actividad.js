319f1ea0aaaf735a9be256d2f76b961afc885d04
Sergio Panigua <xpanisergio@gmail.com>
2023-06-14 20:26:55 +0200

    arreglos

---
+++ components/Actividad.js
@@ -0,0 +1,47 @@
+'use client'
+import { useParams } from 'next/navigation';
+import style from '../styles/actividad.module.scss';
+import Image from 'next/image';
+import { getAllActividades } from '@/app/lib/cms';
+
+const Actividad = async () => {
+
+    //cojer el id de la url
+
+    const { actividad } = useParams();
+
+
+    const actividades = await getAllActividades(actividad);
+
+    return (
+        <div className={style.actividad_container}>
+
+            {
+                actividades.map(actividad => {
+                    return (
+                        <div key={actividad.id} className={style.actividad}>
+                            <h1>{actividad.title}</h1>
+                            <p>{actividad.descripcion}</p>
+                            <Image src={actividad.imagen} alt={actividad.title}
+                                width={400} height={250} />
+                            {
+                                actividad.puntos.map(punto => {
+                                    return (
+                                        <div key={punto.title}>
+                                            <h2>{punto.title}</h2>
+                                            <p>{punto.descripcion}</p>
+                                        </div>
+
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                }
+                )
+            }
+        </div>
+    )
+}
+
+export default Actividad
--- src/app/posts/[actividad]/page.js
+++ src/app/posts/[actividad]/page.js
@@ -1,47 +1,14 @@
-'use client'
+
 import 'styles/global.scss';
 
-import { getAllActividades } from "../../lib/cms";
-import { useParams } from 'next/navigation';
-import style from '../../../../styles/actividad.module.scss';
-import Image from 'next/image';
+
+import Actividad from '../../../../components/Actividad';
 const page = async () => {
 
-    //cojer el id de la url
-
-    const { actividad } = useParams();
-
-
-    const actividades = await getAllActividades(actividad);
-
     return (
-        <div className={style.actividad_container}>
-
-            {
-                actividades.map(actividad => {
-                    return (
-                        <div key={actividad.id} className={style.actividad}>
-                            <h1>{actividad.title}</h1>
-                            <p>{actividad.descripcion}</p>
-                            <Image src={actividad.imagen} alt={actividad.title}
-                                width={400} height={250} />
-                            {
-                                actividad.puntos.map(punto => {
-                                    return (
-                                        <div key={punto.title}>
-                                            <h2>{punto.title}</h2>
-                                            <p>{punto.descripcion}</p>
-                                        </div>
-
-                                    )
-                                })
-                            }
-                        </div>
-                    )
-                }
-                )
-            }
-        </div>
+        <>
+            <Actividad />
+        </>
     )
 }
 
